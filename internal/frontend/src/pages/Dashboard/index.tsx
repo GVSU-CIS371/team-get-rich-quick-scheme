f26a456f402357a562cda5bba71ea51889319bbe
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2026-04-27 07:23:34 -0400

    Finish organizations page

--- internal/frontend/src/pages/Dashboard/index.tsx
+++ internal/frontend/src/pages/Dashboard/index.tsx
@@ -1,13 +1,55 @@
 import {DashboardLayout} from "@/components/ui/dashboard-layout";
+import {useAuth} from "@/components/auth/Auth";
+import {useState} from "preact/hooks";
+import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {Button} from "@/components/ui/button";
+import {useLocation} from "preact-iso";
 
 export function DashboardHomePage() {
+    const {authClient} = useAuth();
+    const [organizations, setOrganizations] = useState([]);
+    const location = useLocation();
+
+    authClient.get('/api/v1/organizations').then(r => {
+       setOrganizations(r.data.data.organizations);
+    });
+
     return (
         <DashboardLayout>
             <div className="rounded-2xl border border-zinc-800 bg-zinc-950 p-6">
                 <h2 className="text-lg font-medium">Organizations</h2>
-                <p className="mt-2 text-sm text-zinc-400">
-                    <p>You currently have no organizations</p>
-                </p>
+                {organizations.length === 0 && (
+                    <p className="mt-2 text-sm text-zinc-400">
+                        You currently have no organizations
+                    </p>
+                )}
+
+                {organizations.length !== 0 && (
+                    <div className="mt-4">
+                        <Table>
+                            <TableCaption>A list of your organizations</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Name</TableHead>
+                                    <TableHead>Description</TableHead>
+                                    <TableHead>Actions</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {organizations.map(org => (
+                                   <TableRow key={org.ID}>
+                                       <TableCell>{org.Name}</TableCell>
+                                       <TableCell>{org.Description}</TableCell>
+                                       <TableCell>
+                                           <Button onClick={() =>
+                                               location.route(`/dashboard/organizations/${org.ID}`)}>View</Button>
+                                       </TableCell>
+                                   </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </div>
+                )}
             </div>
         </DashboardLayout>
     )
